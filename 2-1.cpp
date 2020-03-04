d259cbdc4f3541c454b91c21ce1518069bb843fc
DashaSarnatskaya <58254065+DashaSarnatskaya@users.noreply.github.com>
2020-03-04 23:10:59 +0300

    04.03.2020

--- 2-1.cpp
+++ 2-1.cpp
@@ -4,24 +4,27 @@
 using namespace std;
 int** createArray(int N, int M);
 void freeMemory(int**& p, int N);
-void initRandomArray(int** p, int N, int M, const int min , const int max);
-void initArray(int** p, int N, int M, const int m );
+void initRandomArray(int** p, int N, int M, const int min, const int max);
+void initArray(int** p, int N, int M, const int m);
 void printArray(int** p, int N, int M);
-void sortByMaxInRow(int** p, int M, int N);
+void findMaxInRow(int** p, int N, int M, int* maxarray);
+void sortByMaxInRow(int** p, int N, int M);
 
-/*В массиве А(N,М) расположить строки в порядке возрастания их максимальных элементов.*/
+//В массиве А(N, М) расположить строки в порядке возрастания их максимальных элементов.
 
 int main()
 {
 	int M, N;
 	cout << "Enter rows,cols\n";
-	cin >> M >> N;
-	int** arr = createArray(M, N);
-	initRandomArray(arr, M, N, -10,20);
-	printArray(arr, M, N);
+	cin >> N >> M;
+	int** A = createArray(N, M);
+	initRandomArray(A, N, M, -10, 10);
+	printArray(A, N, M);
 	cout << "\n";
-	sortByMaxInRow(arr, M, N);
-	printArray(arr, M, N);
+	sortByMaxInRow(A, N, M);
+	cout << "\n";
+	printArray(A, N, M);
+	freeMemory(A, N);
 
 }
 int** createArray(int N, int M) {
@@ -38,48 +41,51 @@ void freeMemory(int**& p, int N) {
 	delete[] p;
 	p = nullptr;
 }
-void initRandomArray(int** p, int N, int M, const int min , const int max) {
+void initRandomArray(int** p, int N, int M, const int min, const int max) {
 	int d = (max - min + 1);
 	for (size_t i = 0; i < N; i++)
 		for (size_t j = 0; j < M; j++) p[i][j] = rand() % d + min;
 }
-void initArray(int** p, int N, int M, const int m ) {
+void initArray(int** p, int N, int M, const int m) {
 	for (size_t i = 0; i < N; i++)
 		for (size_t j = 0; j < M; j++) p[i][j] = m;
 }
 void printArray(int** p, int N, int M) {
 	for (size_t i = 0; i < N; i++) {
 		for (size_t j = 0; j < M; j++)
-			cout << setw(3) << p[i][j] << ";";
+			cout << setw(3) << p[i][j] << " ";
 		cout << '\n';
 	}
 }
-void sortByMaxInRow(int** p,  int M,  int N)
+void findMaxInRow(int** p, int N, int M, int*maxarray)
 {
-	int* max = createArray(N);
-	int temp = max[0];
-	for (int i = 0; i < M; i++)
+	int max = **p;
+	for (size_t i = 0; i <N ; i++)
 	{
-		max[i] = p[i][0];
-		for (int j = 1; j < N; j++)
-			if (p[i][j] > max[i])
-				max[i] = p[i][j];
-	}
-	for (int i = 0; i < M - 1; i++)
-		for (int k = i + 1; k < M; k++)
-			if (max[k] < max[i])
-			{
-				
-				temp = max[i];
-				max[i] = max[k];
-				max[k] = temp;
-				for (int j = 0; j < N; j++)
-				{
-					temp = p[i][j];
-					p[i][j] = p[k][j];
-					p[k][j] = temp;
-				}
-			}
-	freeMemory(max);
-}
+		max = p[i][0];
+		for (size_t j = 0; j < M; j++)
+		{
+			if (max < p[i][j])
+				max = p[i][j];
 
+		}
+		maxarray[i] = max;
+		cout <<"\n max element: " << max <<"\n";
+	}
+}
+void sortByMaxInRow(int** p, int N, int M)
+{
+	int* maxarray = createArray(N);
+	findMaxInRow(p, N, M, maxarray);
+	cout << '\n';
+	for (int i = 0; i < N; ++i)
+	{
+		for( int j=0;j<N - 1;++j)
+			if (maxarray[j] > maxarray[j + 1])
+			{
+				swap(maxarray[j], maxarray[j + 1]);	
+				swap(p[j], p[j + 1]);
+
+			}
+	}
+}
