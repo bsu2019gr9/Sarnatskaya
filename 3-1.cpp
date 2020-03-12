a762391b8c42cdd2d5da89490ba6abdf813688bf
DashaSarnatskaya <58254065+DashaSarnatskaya@users.noreply.github.com>
2020-03-13 01:29:30 +0300

    13.03.2020

---
+++ 3-1.cpp
@@ -0,0 +1,36 @@
+#define  _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+#include <cstring>
+using namespace std;
+void countWords(char* str, char letter);
+
+//Определить количество слов в строке, которые начинаются на заданную букву.Вывести их на экран.
+
+int main()
+{
+	char* str = nullptr;
+	str = new(nothrow) char[255];
+	cout << "Enter string\n";
+	cin.getline(str, 255);
+	char letter;
+	cout << "Enter letter\n";
+	cin >> letter;
+	countWords(str, letter);
+	delete[] str;
+	str = nullptr;
+	system("pause");
+	return 0;
+}
+void countWords(char* str, char letter)
+{
+	char* word = strtok(str, " .,:;`*/'#&!");
+	int count = 0;
+	while (word != NULL) {
+		if (word[0] == letter) {
+			cout << word << " ";
+			++count;
+		}
+		word = strtok(NULL, " .,:;`*/'#&!");
+	}
+	cout << '\n' << "Count of word starting with  letter " << letter << ", " << count << '\n';
+}
